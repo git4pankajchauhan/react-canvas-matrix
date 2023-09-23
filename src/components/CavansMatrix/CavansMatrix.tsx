6d213abdfcdb061cb061fc92bf4aed5b54bf01da
Pankaj Singh Chauhan <pankaj.chauhan@neosoftmail.com>
2023-09-23 15:35:19 +0530

    basic file setup

---
+++ src/components/CavansMatrix/CavansMatrix.tsx
@@ -0,0 +1,9 @@
+import classes from "./CavansMatrix.module.css";
+import { IProps } from "./CavansMatrix.utils";
+
+const CavansMatrix = (props: IProps) => {
+  const {} = props;
+  return <section className={classes.rootContainer}>CavansMatrix</section>;
+};
+
+export default CavansMatrix;
